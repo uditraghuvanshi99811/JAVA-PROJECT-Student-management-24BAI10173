0275938012d1c4ad17b6db7145c2e7982846f9e1
uditraghuvanshi99811 <uditraghuvanshi99811@gmail.com>
2026-03-31 22:58:41 +0530

    Refactor Student class with variable renaming

--- Student.java
+++ Student.java
@@ -1,63 +1,71 @@
+
 class Student extends Person {
-    private int regNo;
-    private String email;
-    private String course;
-    private String parentName;
-    private String parentContact;
+   private int regNo;
+   private String email;
+   private String course;
+   private String parentName;
+   private String parentContact;
+   private int[] marks = new int[5];
+   private double percentage;
+   private String grade;
+   private double cgpa;
 
-    private int[] marks = new int[5];
-    private double percentage;
-    private String grade;
-    private double cgpa;
+   public Student(int var1, String var2, int var3, String var4, String var5, String var6, String var7, int[] var8, double var9) {
+      super(var2, var3);
+      this.regNo = var1;
+      this.email = var4;
+      this.course = var5;
+      this.parentName = var6;
+      this.parentContact = var7;
+      this.marks = var8;
+      this.cgpa = var9;
+      this.calculateResult();
+   }
 
-    public Student(int regNo, String name, int age, String email,
-                   String course, String parentName, String parentContact,
-                   int[] marks, double cgpa) {
+   private void calculateResult() {
+      int var1 = 0;
 
-        super(name, age);
-        this.regNo = regNo;
-        this.email = email;
-        this.course = course;
-        this.parentName = parentName;
-        this.parentContact = parentContact;
-        this.marks = marks;
-        this.cgpa = cgpa;
+      for(int var5 : this.marks) {
+         var1 += var5;
+      }
 
-        calculateResult();
-    }
+      this.percentage = (double)var1 / (double)5.0F;
+      if (this.percentage >= (double)90.0F) {
+         this.grade = "A";
+      } else if (this.percentage >= (double)75.0F) {
+         this.grade = "B";
+      } else if (this.percentage >= (double)50.0F) {
+         this.grade = "C";
+      } else {
+         this.grade = "Fail";
+      }
 
-    private void calculateResult() {
-        int total = 0;
-        for (int m : marks) {
-            total += m;
-        }
-        percentage = total / 5.0;
+   }
 
-        if (percentage >= 90) grade = "A";
-        else if (percentage >= 75) grade = "B";
-        else if (percentage >= 50) grade = "C";
-        else grade = "Fail";
-    }
+   public int getRegNo() {
+      return this.regNo;
+   }
 
-    public int getRegNo() { return regNo; }
-    public double getPercentage() { return percentage; }
+   public double getPercentage() {
+      return this.percentage;
+   }
 
-    public void display() {
-        System.out.println("\n----- Student Details -----");
-        System.out.println("Reg No: " + regNo);
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-        System.out.println("Email: " + email);
-        System.out.println("Course: " + course);
-        System.out.println("Parent: " + parentName + " (" + parentContact + ")");
-        System.out.println("CGPA: " + cgpa);
+   public void display() {
+      System.out.println("\n----- Student Details -----");
+      System.out.println("Reg No: " + this.regNo);
+      System.out.println("Name: " + this.name);
+      System.out.println("Age: " + this.age);
+      System.out.println("Email: " + this.email);
+      System.out.println("Course: " + this.course);
+      System.out.println("Parent: " + this.parentName + " (" + this.parentContact + ")");
+      System.out.println("CGPA: " + this.cgpa);
+      System.out.print("Marks: ");
 
-        System.out.print("Marks: ");
-        for (int m : marks) {
-            System.out.print(m + " ");
-        }
+      for(int var4 : this.marks) {
+         System.out.print(var4 + " ");
+      }
 
-        System.out.println("\nPercentage: " + percentage);
-        System.out.println("Grade: " + grade);
-    }
+      System.out.println("\nPercentage: " + this.percentage);
+      System.out.println("Grade: " + this.grade);
+   }
 }
